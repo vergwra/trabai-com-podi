203cc8c75d79d9acad178711a026b7822a310014
vergwra <vergwraaa@gmail.com>
2023-12-06 12:57:49 -0300

    miau mds do ceu amo meu pincesu nindo e pinceso de novo

---
+++ src/Abstratos/Pagamento.java
@@ -0,0 +1,33 @@
+package Abstratos;
+
+public abstract class Pagamento {
+    protected double valor;
+    protected String formaDePagamento;
+    
+    public Pagamento(double valor, String formaDePagamento) {
+        this.valor = valor;
+        this.formaDePagamento = formaDePagamento;
+    }
+
+    public abstract boolean processarPagamento();
+
+    public String toString() {
+        return "Pagamento no valor de=" + valor + "";
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getFormaDePagamento() {
+        return formaDePagamento;
+    }
+
+    public void setFormaDePagamento(String formaDePagamento) {
+        this.formaDePagamento = formaDePagamento;
+    }
+}
